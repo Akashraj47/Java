06afe106fa66fcf6cc1862c25e799f3b536fe2e8
Akashraj47 <91429658+Akashraj47@users.noreply.github.com>
2022-04-09 14:46:37 +0530

    Add files via upload

---
+++ lab/super.java
@@ -0,0 +1,30 @@
+class xyz {
+	int x,y,z;
+	xyz(int x,int y,int z){
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+	void show(){
+		System.out.println("x = "+x+"Y="+y+"Z"+z);
+	}
+}
+class abc extends xyz{
+	int a ,b;
+	abc(int a,int b,int c,int d,int e){
+		super(c,d,e);
+		this.a = a;
+		this.b = b;
+		
+	}
+	void show(){
+		super.show();
+		System.out.println("a = "+a+"b ="+b);
+	}
+}
+class User{
+	public static void main(String []args){
+	abc c = new abc(2,3,4,5,6);
+	c.show();
+	}
+}
